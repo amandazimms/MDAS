cd888e8f62dd37e2316d30a7905e33644cda351c
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-09-23 14:22:52 -0500

    added more comments- base mode complete except readme update, which I'll save until after stretch goals

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,12 @@
 $(document).ready(onReady);
 
+//global vars used to store the operator button info, since in this project,
+//when an operator button is pressed it remains 'down' until an equation is calculated
 let operatorSelected;
 let operators = ['+', '-', '*', '/'];
 
 function onReady(){
+  //setup math operator, submit, and clear buttons and what they do
   $( '#submit-button' ).on( 'click', submitEquation )
 
   $( '#add-button' ).on( 'click', selectAddAsOperator );
@@ -16,72 +19,71 @@ function onReady(){
 
 //THIS IS NOT DRY but I can't figure out how to pass a parameter with .on('click');
 //and I don't care much because the stretch goals look very different anyway
-function selectAddAsOperator() {
+function selectAddAsOperator() { //update global var & visuals to show that the add button is 'down' 
   operatorSelected = operators[0]; 
   selectedButtonDarker( $(this) );
 }
 
-function selectSubtractAsOperator() {
+function selectSubtractAsOperator() { //update global var & visuals to show that the subtract button is 'down'
   operatorSelected = operators[1]; 
   selectedButtonDarker( $(this) );
 }
 
-function selectMultiplyAsOperator() {
+function selectMultiplyAsOperator() { //update global var & visuals to show that the multiply button is 'down'
   operatorSelected = operators[2]; 
   selectedButtonDarker( $(this) );
 }
 
-function selectDivideAsOperator() {
+function selectDivideAsOperator() { //update global var & visuals to show that the divide button is 'down'
   operatorSelected = operators[3];
   selectedButtonDarker( $(this) );
 }
 
 function selectedButtonDarker(theThis){
   //function to make all buttons except 'this' go back to their normal color,
-  //and make 'this' button darker
+  //and make 'this' button darker - 
+  //shows user which math operator button is 'down' and ready to use for calculating
   theThis.siblings('.math-button').css('background-color', '#C7D4DA');
   theThis.css('background-color', '#7FA5B6');
 }
 
 function clearFields(){
-  //function clear input fields of numbers
+  //function to clear input fields of numbers
   $( '.num-input' ).val('')
+  //also 'clears' the 'down' effect of any operator buttons pushed
   selectedButtonDarker( $(this) );
 }
 
 
 function submitEquation() {
-  //function that collects inputs to form an equation and sends them to server
+  //function that collects inputs, puts them in an object, and sends them to server
   //it does not do the actual calculating (the server does).
 
-  let firstNum = $('#first-num-input').val();
-  let secondNum = $('#second-num-input').val();
-
-  console.log('operators?', operators);
-  console.log('operatorSelected?', operatorSelected);
+  let firstNum = $('#first-num-input').val(); //get number values
+  let secondNum = $('#second-num-input').val(); //get number values
 
   let equationToSend = {
     firstNumber: firstNum,
-    operator: operatorSelected,
+    operator: operatorSelected, //didn't need to 'get' this since it's global
     secondNumber: secondNum,
     answer: 0
   }
 
-  $.ajax ({
+  $.ajax ({ //hey ajax...
 
-    method: 'POST',
-    url: '/equations',
-    data: equationToSend
+    method: 'POST', //do a "POST" to server
+    url: '/equations', //specifically, on the /equations area
+    data: equationToSend //send it our equation object
 
-  }).then ( function(response) {
+  }).then ( function(response) { //if that was successful...
 
-    getEquations();//if successful... update Dom
-    $( '.num-input' ).val(''); //clear input fields
-    selectedButtonDarker( $(this) ); //clear operator button and make this the dark button instead
+    getEquations(); //update the DOM with the history of previous equations (including this one)
+    $( '.num-input' ).val(''); //clear user input fields so they can enter another equation
+    selectedButtonDarker( $(this) ); //clear whichever operator button was 'down' and make '=' the dark button instead
 
-  }).catch ( function(err) {
+  }).catch ( function(err) { //if that was not successful...
 
-    alert('error sending equation');
+    alert('error sending equation'); //alert us
     console.log('error:', err);
   });
 }
@@ -90,26 +92,25 @@ function getEquations() {
   //function that gets all the equations we've done from the server
   //and displays them on the dom like any normal calculator would ;)
 
-  $.ajax ({
+  $.ajax ({ //hey ajax...
 
-    method: 'GET',
-    url: '/equations'
+    method: 'GET', //do a "GET" from server
+    url: '/equations' //specifically, on the /equations area
 
-  }).then ( function(response) {
+  }).then ( function(response) { //if that was successful...
 
-    let equationHistory = $("#equation-history");
-    equationHistory.empty();
+    let equationHistory = $("#equation-history"); //get the output area where we want to display things on the DOM
+    equationHistory.empty(); //empty it out from whatever was appended there last time, so we don't have any duplicating
 
-    for (let i=0; i<response.length; i++)
-    {
-      equationHistory.append( 
+    for (let i=0; i<response.length; i++) { //for (whatever we got back from the server - our history of equations)
+      equationHistory.append( //append them to this appropriate area
         `<li>${response[i].firstNumber} ${response[i].operator} ${response[i].secondNumber} = ${response[i].answer}</li>
         `);
     };
 
-  }).catch( function(err) {
+  }).catch( function(err) { //if that was not successful...
 
-    alert('error getting equation history');
+    alert('error getting equation history'); //alert us
     console.log('error:', err)
 
   });
--- server/server.js
+++ server/server.js
@@ -1,60 +1,58 @@
-//requires
+//REQUIRES
 let express = require('express'); //think of this as server-side version of jquery or something similar
 let app = express(); //now app is going to use everything that is returned after we run express. think of 'app' as how jquery uses '$'
-//NEEDED for POSTS to work:
-let bodyParser = require('body-parser');
+let bodyParser = require('body-parser'); //NEEDED for POSTS to work
 
-// uses
+//USES
 app.use( express.static( 'server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
-//global vars
+//GLOBALS
 const port = 5000; 
 let equationHistory = [];
 
-//spin up server
-app.listen(port, ()=> { //we will use arrow functions on server side
-  console.log('server is up on', port);
+//SPIN UP THAT SERVER
+app.listen(port, ()=> { //we use arrow functions on server side
+  console.log('server is up on', port); 
 });
 
 
-//GET ROUTES
+//ROUTES
 
-//HOMEPAGE
+//HOMEPAGE-GET
 app.get('/', (req, res) => { //our 2 arguments req and res (remember they're alphabetical!) to pass to this arrow function...
   console.log('get route hit'); //anytime someone goes to localhost:5000 in their browser, this will appear in our console here.
-  //res.send('meow'); //anytime someone goes to localhost:5000 in their browser, they will see this on the page
 });
 
-
 //EQUATIONS-GET
-app.get( '/equations', ( req, res ) => {
-  console.log("i'm about to send this equation history:", equationHistory);
-  res.send( equationHistory );
+app.get( '/equations', ( req, res ) => { //if I understand correctly, this is the mirror-image of our ajax "GET" to /equations...
+  res.send( equationHistory );          //...you need both for it to work
 });
 
 
 //EQUATIONS-POST
-app.post( '/equations', ( req, res ) => {
-  res.sendStatus( 200 );
+app.post( '/equations', ( req, res ) => { //if I understand correctly, this is the mirror-image of our ajax "POST" to /equations...
+  res.sendStatus( 200 );                  //...you need both for it to work
 
-  //req.body is the following object!  { firstNumber: '3', secondNumber: '1', operator: '+' }
+  //FYI! req.body is the following object!  { firstNumber: '3', secondNumber: '1', operator: '+' }
+
+  //do the math!
   switch (req.body.operator){
-    case '+':
-      req.body.answer = Number(req.body.firstNumber) + Number(req.body.secondNumber);
+    case '+': //if it was a plus...
+      req.body.answer = Number(req.body.firstNumber) + Number(req.body.secondNumber); //add them
       break;
-    case '-':
+    case '-': //same for minus
       req.body.answer = Number(req.body.firstNumber) - Number(req.body.secondNumber);
       break;
-     case '*':
+     case '*': //multiply
       req.body.answer = Number(req.body.firstNumber) * Number(req.body.secondNumber);
       break;
-    case '/':
+    case '/': //divide
       req.body.answer = Number(req.body.firstNumber) / Number(req.body.secondNumber);
       break;
-    default:
+    default: //if something else...?
       console.log('error: invalid operator');
   }
 
-  equationHistory.push(req.body);
+  equationHistory.push(req.body); //push the resulting object into our equation history array!
 });
