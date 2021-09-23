13d42a114fb38bf05afa21906214c4b33df855db
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-09-23 11:58:53 -0500

    everyone is talking to each other! index.html referenced, js and jquery sourced, style linked, express installed, server running on port 5000.

---
+++ server/server.js
@@ -0,0 +1,27 @@
+//requires
+let express = require('express'); //think of this as server-side version of jquery or something similar
+let app = express(); //now app is going to use everything that is returned after we run express. think of 'app' as how jquery uses '$'
+//NEEDED for POSTS to work:
+let bodyParser = require('body-parser');
+
+// uses
+app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+//global vars
+const port = 5000; 
+
+
+//spin up server
+app.listen(port, ()=> { //we will use arrow functions on server side
+  console.log('server is up on', port);
+});
+
+
+//GET ROUTES
+
+//HOMEPAGE
+app.get('/', (req, res) => { //our 2 arguments req and res (remember they're alphabetical!) to pass to this arrow function...
+  console.log('get route hit'); //anytime someone goes to localhost:5000 in their browser, this will appear in our console here.
+  res.send('meow'); //anytime someone goes to localhost:5000 in their browser, they will see this on the page
+});
